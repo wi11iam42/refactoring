640083198c28e432f97b9d13e846b1c18e6cf81d
Your Name <william.ignasiak@gmail.com>
2025-11-21 14:33:42 -0500

    Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,8 +31,6 @@ public class StatementPrinter {
 
         for (Performance p : invoice.getPerformances()) {
 
-            final int rslut = getAmount(p);
-
             // add volume credits
             volumeCredits += Math.max(p.audience - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -41,8 +39,8 @@ public class StatementPrinter {
             }
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(p).getName(),
-                    frmt.format(rslut / Constants.PERCENT_FACTOR), p.audience));
-            totalAmount += rslut;
+                    frmt.format(getAmount(p) / Constants.PERCENT_FACTOR), p.audience));
+            totalAmount += getAmount(p);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
